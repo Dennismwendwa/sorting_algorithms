ec317444d0d1702c257ad1909536e4f7b8ecb76e
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-19 20:38:13 +0300

    created the buble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,9 +12,10 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t k;
 	int swap = -1;
-	int passes = 0;
 	int temp;
-	
+
+
+
 	if (size < 2)
 		return;
 
@@ -35,6 +36,5 @@ void bubble_sort(int *array, size_t size)
 			}
 		}
 
-		passes++;
 	}
 }
